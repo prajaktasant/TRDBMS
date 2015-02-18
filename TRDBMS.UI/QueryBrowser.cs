ebe26f30159281c87d539383b9007d4ea325bb21
prajaktasant <prajakta.uoh@gmail.com>
2015-02-17 23:37:07 -0600

    Initial TRDBMS Commit

---
+++ TRDBMS.UI/QueryBrowser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TRDBMS.UI
+{
+    public partial class QueryBrowser : Form
+    {
+        public QueryBrowser()
+        {
+            InitializeComponent();
+        }
+    }
+}
